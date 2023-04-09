d3f0cc40505a04437bd6c7f261bf3d1aaf81f130
AlexTigra1974 <106554517+AlexTigra1974@users.noreply.github.com>
2023-04-09 18:32:04 +0300

    fix2

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { toast } from 'react-toastify';
 import { BsSearch } from 'react-icons/bs';
 import css from './Searchbar.module.css';
