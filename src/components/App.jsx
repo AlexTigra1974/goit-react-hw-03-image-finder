a2e81bcc6c74924a691d6e7513f6d13490307130
AlexTigra1974 <106554517+AlexTigra1974@users.noreply.github.com>
2023-04-09 18:23:58 +0300

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,97 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template3
-    </div>
-  );
-};
+import { Component } from 'react';
+import { FetchApi } from 'servises/pixabayAPI';
+import { toast, ToastContainer } from 'react-toastify';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Searchbar } from './Searchbar/Searchbar';
+import { Modal } from 'components/Modal/Modal';
+import { Loader } from './Loader/Loader';
+import { Button } from './Button/Button';
+
+import css from './App.module.css';
+
+export class App extends Component {
+  state = {
+    searchText: '',
+    hits: [],
+    page: 1,
+    totalHits: 0,
+    largeImage: '',
+    loading: false,
+    showModal: false,
+  };
+
+  componentDidUpdate(_, prevState) {
+    if (
+      prevState.searchText !== this.state.searchText ||
+      prevState.page !== this.state.page
+    ) {
+      // fetch()
+      this.FetchImages();
+      // console.log(prevProps.searchText);
+      // console.log(this.props.searchText);
+    }
+  }
+
+  FetchImages = async () => {
+    const { searchText, page } = this.state;
+    this.setState({ loading: true, totalHits: 0 });
+    try {
+      const { hits, totalHits } = await FetchApi(searchText, page);
+      console.log(hits);
+      console.log(totalHits);
+
+      this.setState(() => ({
+        hits: [...this.state.hits, ...hits],
+        totalHits: totalHits,
+      }));
+    } catch (err) {
+      console.log(err);
+      toast('Something went wrong...');
+      this.setState({ loading: false });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  handleFormSubmit = search => {
+    console.log(search);
+    this.setState({ searchText: search, hits: [], page: 1 });
+  };
+
+  onLoadMore = () => {
+    this.setState(({ page }) => ({
+      page: page + 1,
+    }));
+  };
+
+  createLargeImage = largeImageURL => {
+    console.log(largeImageURL);
+    this.setState({ largeImage: largeImageURL, showModal: true });
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
+  render() {
+    const { hits, totalHits, largeImage, showModal, loading } = this.state;
+    const { handleFormSubmit, onLoadMore, createLargeImage, toggleModal } =
+      this;
+    return (
+      <div className={css.App}>
+        <Searchbar createSearchText={handleFormSubmit} />
+        <ImageGallery hits={hits} createLargeImage={createLargeImage} />
+
+        {loading && <Loader />}
+        {showModal && (
+          <Modal largeImageURL={largeImage} onClick={toggleModal} />
+        )}
+
+        {totalHits > 12 && <Button onClick={onLoadMore} />}
+        <ToastContainer position="top-center" autoClose={3000} theme="light" />
+      </div>
+    );
+  }
+}
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,57 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
+import { BsSearch } from 'react-icons/bs';
+import css from './Searchbar.module.css';
+
+export class Searchbar extends Component {
+  state = {
+    search: '',
+  };
+
+  handleSearch = ({ currentTarget: { value } }) => {
+    this.setState({ search: value.toLowerCase() });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    if (this.state.search.trim() === '') {
+      return toast.error('Сформуйте запит для пошуку');
+    }
+    this.props.createSearchText({ search: '' });
+    // console.log(this.state);
+    // console.log(this.props);
+    // this.setState({ search: '' });
+  };
+
+  render() {
+    return (
+      <header className={css.searchbar}>
+        <form className={css.searchForm} onSubmit={this.handleSubmit}>
+          <input
+            className={css.input}
+            type="text"
+            name=" search"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={this.state.search}
+            onChange={this.handleSearch}
+          />
+          <button type="submit" className={css.btn}>
+            <BsSearch size="25" />
+            <span className={css.label}>Search</span>
+          </button>
+        </form>
+      </header>
+    );
+  }
+}
+
+// Searchbar.propTypes = {
+//   //   HandleSubmit: PropTypes.func.isRequired,
+//   //   handleSearch: PropTypes.func.isRequired,
+
+//   search: PropTypes.string.isRequired,
+// };
